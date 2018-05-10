0f510d7e945d2c3cdc4a098785a06f24271e8a67
kartheek Alluri <kartheek.alluri@rrd.com>
2018-05-10 21:27:22 +0530

    new branch for Sub operation
    new branch for Sub operation

---
+++ CalculatorLib/Substract.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculatorLib
+{
+    public class Substract
+    {
+        public int SubOperation(int x, int y)
+        {
+            return x - y;
+        }
+    }
+}
